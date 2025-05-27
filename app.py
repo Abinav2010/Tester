aeefbe0f3fe45335f7681c4d549fe5b8ed1e0824
Abinav2010 <94927833+Abinav2010@users.noreply.github.com>
2025-05-27 11:53:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello, World from Jenkins Docker Pipeline!"
+def home():
+    return "Flask app is running!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    # Bind to 0.0.0.0 so it’s accessible outside the Docker container
+    app.run(host="0.0.0.0", port=5000)
